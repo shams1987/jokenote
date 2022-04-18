605adc49d178902c5e9a2f092c3bcbe5e580c166
shams <shamsjshaikh@gmail.com>
2022-04-18 16:07:30 -0500

    fixed home page rating dropdown

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllJokesThunk } from "../../store/joke"
 
@@ -10,6 +10,32 @@ const HomePage = () => {
     const allJokesList = useSelector(state => Object.values(state.joke));
     const sessionUser = useSelector(state => state.session.user);
     const userId = sessionUser.id;
+    const [rating, setRating] = useState("desc");
+
+
+    function bblSort(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0; j < (arr.length - i - 1); j++) {
+                if (arr[j].rating < arr[j + 1].rating) {
+                    let temp = arr[j]
+                    arr[j] = arr[j + 1]
+                    arr[j + 1] = temp
+                }
+            }
+        }
+        return arr;
+    }
+
+    // select order of jokes by rating
+    let sortedJokes;
+
+    if (rating === "desc") {
+        sortedJokes = bblSort(allJokesList);
+    }
+    else {
+        sortedJokes = bblSort(allJokesList).reverse();
+    }
+
 
     useEffect(() => {
         dispatch(getAllJokesThunk(userId));
@@ -27,7 +53,19 @@ const HomePage = () => {
     return (
         <div>
             <div><h1>You have {allJokesList.length} jokes with average rating of {avgRating}/5</h1></div>
-            {allJokesList?.map(joke => (
+            <div>
+                <label>
+                    Order
+                    <select
+                        value={rating}
+                        onChange={e => setRating(e.target.value)}
+                    >
+                        <option value="desc">highest rated</option>
+                        <option value="asc">lowest rated</option>
+                    </select>
+                </label>
+            </div>
+            {sortedJokes?.map(joke => (
                 <div key={joke.id}>
                     <ul>
                         <li key={joke.id + "A"}>
